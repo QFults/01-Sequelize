65e79cb62d958141c2d131b67966844d475d1ad9
Quinton Fults <quinton.fults.work@gmail.com>
2020-12-02 13:33:12 -0800

    code

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,21 @@
 const router = require('express').Router()
 const user = require('../models/user.js')
 
-router.get('/users', (req, res) => user.getAll(users => res.json(users)))
 
-router.post('/users', (req, res) => user.create(req.body, () => res.sendStatus(200)))
+router.get('/users', (req, res) => 
+  user.getAll(users => 
+    res.render('index', { users })))
 
-router.put('/users/:id', (req, res) => user.update(req.body, req.params.id, () => res.sendStatus(200)))
+router.post('/users', (req, res) => 
+  user.create(req.body, () => 
+    res.sendStatus(200)))
 
-router.delete('/users/:id', (req, res) => user.delete(req.params.id, () => res.sendStatus(200)))
+router.put('/users/:id', (req, res) => 
+  user.update(req.body, req.params.id, () => 
+    res.sendStatus(200)))
+
+router.delete('/users/:id', (req, res) => 
+  user.delete(req.params.id, () => 
+    res.sendStatus(200)))
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require('express')
+const { join } = require('path')
 
 const app = express()
 
+app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.engine('.hbs', require('express-handlebars')({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', '.hbs')
 
-app.get('/', (req, res) => {
-  res.render('index', { name: 'John Doe', age: 47 })
-})
+// app.get('/', (req, res) => {
+//   res.render('index', { name: 'John Doe', age: 47 })
+// })
 
-app.get('/jane', (req, res) => {
-  res.render('index', { name: 'Jane Doe', age: 46 })
-})
+// app.get('/jane', (req, res) => {
+//   res.render('index', { name: 'Jane Doe', age: 46 })
+// })
 
 app.use(require('./controllers'))
 
