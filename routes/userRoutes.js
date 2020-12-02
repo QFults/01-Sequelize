2b224db7d173e5c65bfcef891b5d4066f1cf7c9c
Quinton Fults <quinton.fults.work@gmail.com>
2020-12-02 11:02:41 -0800

    code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,33 +1,20 @@
 const router = require('express').Router()
-const mysql = require('mysql2')
-const db = mysql.createConnection('mysql://root:rootroot@localhost/users_db')
+const orm = require('../config/orm.js')
 
 router.get('/users', (req, res) => {
-  db.query('SELECT * FROM users', (err, users) => {
-    if (err) { console.log(err) }
-    res.json(users)
-  })
+  orm.getAll('users', users => res.json(users))
 })
 
 router.post('/users', (req, res) => {
-  db.query('INSERT INTO users SET ?', req.body, err => {
-    if (err) { console.log(err) }
-    res.sendStatus(200)
-  })
+  orm.create('users', req.body, () => res.sendStatus(200))
 })
 
 router.put('/users/:id', (req, res) => {
-  db.query('UPDATE users SET ? WHERE ?', [req.body, { id: req.params.id }], err => {
-    if (err) { console.log(err) }
-    res.sendStatus(200)
-  })
+  orm.update('users', req.body, { id: req.params.id }, () => res.sendStatus(200))
 })
 
 router.delete('/users/:id', (req, res) => {
-  db.query('DELETE FROM users WHERE ?', { id: req.params.id }, err => {
-    if (err) { console.log(err) }
-    res.sendStatus(200)
-  })
+  orm.delete('users', { id: req.params.id }, () => res.sendStatus(200))
 })
 
 module.exports = router
